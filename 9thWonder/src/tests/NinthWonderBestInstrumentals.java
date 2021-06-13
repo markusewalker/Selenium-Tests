3f31d4067eb36da7ffc062f12fdd06221de84e00
Markus Walker <52591819+markusewalker@users.noreply.github.com>
2021-06-13 13:19:56 -0700

    Update NinthWonderBestInstrumentals.java

--- 9thWonder/src/tests/NinthWonderBestInstrumentals.java
+++ 9thWonder/src/tests/NinthWonderBestInstrumentals.java
@@ -33,7 +33,7 @@ public class NinthWonderBestInstrumentals {
 		options.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
 		
 		// BROWSER VARIABLE DECLARATION/INITIALIZATION SECTION
-		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
 		driver = new ChromeDriver(options);
 		wait = new WebDriverWait(driver,20);
 		driver.manage().window().maximize();
